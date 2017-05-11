d4aec6cc2da2a0efbef91608afe7962b098d18dd
Ujwal Baidar <baidar.ujwal@gmail.com>
2017-05-11 14:31:27 +0545

    fixed profile issue

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -40,9 +40,7 @@ module.exports = function(io){
 									resolve({remainingDays: billingResponse.remainingDays, packageType: billingResponse.priorityLevel});
 									// resolve({remainingDays: billingResponse.remainingDays, packageType: billingResponse.packageType});
 								}else{
-									if(billingResponse.remainingDays !== undefined){
-										resolve({remainingDays: billingResponse.remainingDays, packageType: 1});
-									}
+									resolve({remainingDays: billingResponse.remainingDays, packageType: 1});
 									// resolve({remainingDays: 0, packageType: 'Basic'});
 								}
 							}
