ceb77718005a52ed0db41a3e5911b433c4208be0
Ujwal Baidar <baidar.ujwal@gmail.com>
2017-07-03 16:00:46 +0545

    removed files

--- trek-engine-client/src/app/analytics/index.ts
+++
@@ -1,8 +0,0 @@
-export { AnalyticsComponent } from './analytics.component';
-export { AudienceAnalyticsComponent } from './audience-analytics/audience-analytics.component';
-export { AgeAudienceAnalyticsComponent } from './audience-analytics/age-audience-analytics/age-audience-analytics.component';
-export { CountryAudienceAnalyticsComponent } from './audience-analytics/country-audience-analytics/country-audience-analytics.component';
-export { GenderAudienceAnalyticsComponent } from './audience-analytics/gender-audience-analytics/gender-audience-analytics.component';
-export { OverviewAudienceAnalyticsComponent } from './audience-analytics/overview-audience-analytics/overview-audience-analytics.component';
-export { OverviewTripAnalyticsComponent } from './trip-analytics/overview-trip-analytics/overview-trip-analytics.component';
-export { TripBookingTripAnalyticsComponent } from './trip-analytics/trip-booking-trip-analytics/trip-booking-trip-analytics.component';
