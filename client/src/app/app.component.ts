4c90696922e862319645a17715b94433192e54fb
Ujwal Baidar <baidar.ujwal@gmail.com>
2017-08-09 13:50:51 +0545

    upgraded angular cli

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AuthService, UserService } from './services/index';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'trek-engine-app',
+  providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit { 
+	public urlParams:any;
+	public sub: any;
+	constructor(
+		private auth:AuthService, 
+		public userService:UserService, 
+		private _route:Router, 
+		private activatedRoute: ActivatedRoute, 
+		private location: Location,
+		public _cookieService:CookieService
+	){}
+
+	ngOnInit(){
+		this.navigateAppRoute();
+	}
+
+	navigateAppRoute(){
+		let publicPath = ['/login', '/register', '/home', '/forgot-password'];
+		if(JSON.stringify(this._cookieService.getAll()) !== '{}'){
+			if(this._cookieService.get('authToken') && this._cookieService.get('authToken').length>0){
+				if((this.location.path().indexOf('/register/validate?') > -1)){
+					let queryUrl = {};
+					if (location.search) location.search.substr(1).split("&").forEach(item => {let [k,v] = item.split("="); v = v && decodeURIComponent(v); (queryUrl[k] = queryUrl[k] || []).push(v)})
+					let navigateUrl = ['/register/validate',queryUrl['code'][0], queryUrl['loginType'][0], queryUrl['authuser'][0], queryUrl['prompt'][0], queryUrl['session_state'][0]]
+					this._route.navigate(navigateUrl);
+				}else{
+					this.auth.validateToken(this._cookieService.get('authToken'))
+						.subscribe(validationResponse=>{
+							if(JSON.stringify(validationResponse) !== '{}'){
+								if(publicPath.includes(this.location.path()) || (this.location.path().indexOf('/change-password/token/') > -1) || (this.location.path().indexOf('/authorization/token/') > -1) ){
+									this._route.navigate(['/app']);
+								}else{
+									if(this.location && this.location.path() === ''){
+										this._route.navigate(['/app']);
+									}else{
+										this._route.navigate([this.location.path()]);
+									}
+								}
+							}else{
+								this._cookieService.removeAll();
+								window.location.href = environment.webUrl;
+							}
+						}, error =>{
+							this._cookieService.removeAll();
+							window.location.href = environment.webUrl;
+						});
+				}
+			}else{
+				this._cookieService.removeAll();
+				this._route.navigate(['/home']);
+			}
+		}else{
+			if( publicPath.includes(this.location.path()) || (this.location.path().indexOf('/change-password/token/') > -1) || (this.location.path().indexOf('/authorization/token/') > -1) || (this.location.path().indexOf('/register/code/') > -1) ){
+				this._route.navigate([this.location.path()]);
+			}else{
+				if((this.location.path().indexOf('/register/validate?') > -1)){
+					let queryUrl = {};
+					if (location.search) location.search.substr(1).split("&").forEach(item => {let [k,v] = item.split("="); v = v && decodeURIComponent(v); (queryUrl[k] = queryUrl[k] || []).push(v)})
+					let navigateUrl = ['/register/validate',queryUrl['code'][0], queryUrl['loginType'][0], queryUrl['authuser'][0], queryUrl['prompt'][0], queryUrl['session_state'][0]]
+					this._route.navigate(navigateUrl);
+				}else{
+					this._route.navigate(['/home']);
+				}
+			}
+		}
+	}
+}
---
+++ client/src/app/services/notifications.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers, RequestOptions, Response, URLSearchParams } from '@angular/http';
+import { AuthService } from './index';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+
+@Injectable()
+export class NotificationsService {
+	private observable: Observable<any>;
+	constructor(public _cookieService:CookieService, private http: Http, public authService:AuthService) { 
+
+  	}
+  	
+  	getUserNotifications(){
+  		let headers = new Headers({ 'Content-Type': 'application/json', 'token': this._cookieService.get('authToken') });
+    	let options = new RequestOptions({ headers: headers });
+		return this.http.get('/api/notifications/getAllNotifications', options)
+            .map(this.extractData)
+            .catch(this.handleError.bind(this));
+	}
+
+	submitResponse(submitData:object){
+		let headers = new Headers({ 'Content-Type': 'application/json', 'token': this._cookieService.get('authToken') });
+    	let options = new RequestOptions({ headers: headers });
+		return this.http.post('/api/notifications/submitResponse', submitData, options)
+            .map(this.extractData)
+            .catch(this.handleError.bind(this));
+	}
+
+	getUnreadNotifications(){
+		let headers = new Headers({ 'Content-Type': 'application/json', 'token': this._cookieService.get('authToken') });
+    	let options = new RequestOptions({ headers: headers });
+		return this.http.get('/api/notifications/getNewNotifications', options)
+            .map(this.extractData)
+            .catch(this.handleError.bind(this));
+	}
+
+	updateNotificationData(updateObj:object){
+		let headers = new Headers({ 'Content-Type': 'application/json', 'token': this._cookieService.get('authToken') });
+    	let options = new RequestOptions({ headers: headers });
+		return this.http.put('/api/notifications/updateNotificationData', updateObj, options)
+            .map(this.extractData)
+            .catch(this.handleError.bind(this));
+	}
+
+	private extractData(res: Response) {
+    	let body = res.json();
+    	return body.data || { };
+    }
+
+    private handleError (error: Response | any) {
+    	if(error.status == 401){
+    		this.authService.logout();
+			return Observable.of([]);
+    	}else{
+	    	let body: any;
+			let errMsg: string;
+			if (error instanceof Response) {
+				body = error.json() || '';
+				const err = body.error || JSON.stringify(body);
+				errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+			} else {
+				errMsg = error.message ? error.message : error.toString();
+			}
+			return Observable.throw({errMsg:errMsg, errBody: body});
+    	}
+	}
+}
